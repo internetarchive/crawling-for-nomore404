0ab7680ee77451beedec73fd5bd1014dac83f612
Noah Levitt <nlevitt@archive.org>
2013-10-15 01:21:23 +0000

    convert rb'...' to br'...' so it works in python3.2

--- whois/whois-expirations.py
+++ whois/whois-expirations.py
@@ -8,87 +8,87 @@ from datetime import datetime
 import gzip
 
 def parse_date(s):
-    if re.match(rb'^\d{4}-\d{2}-\d{2}(\.?$| .*)', s):
+    if re.match(br'^\d{4}-\d{2}-\d{2}(\.?$| .*)', s):
         # 2014-10-19 (YYYY-MM-DD)
         # 2018-01-15.
         return datetime.strptime(s[:10].decode('utf-8'), '%Y-%m-%d')
-    elif re.match(rb'^\d{2}-[A-Z][a-z][a-z]-\d{4} \d{2}:\d{2}:\d{2} [A-Z]+$', s):
+    elif re.match(br'^\d{2}-[A-Z][a-z][a-z]-\d{4} \d{2}:\d{2}:\d{2} [A-Z]+$', s):
         # 03-May-2015 12:34:26 UTC
         return datetime.strptime(s.decode('utf-8'), '%d-%b-%Y %H:%M:%S %Z')
-    elif re.match(rb'^\d\d\d\d-\d\d-\d\dT\d\d:\d\d:\d\d.\d+Z$', s):
+    elif re.match(br'^\d\d\d\d-\d\d-\d\dT\d\d:\d\d:\d\d.\d+Z$', s):
         # 2012-04-02T14:30:00.000Z ... ignore milliseconds
         # 2014-10-01T00:00:00.0Z ... ignore tenths of second
         return datetime.strptime(s[:19].decode('utf-8'), '%Y-%m-%dT%H:%M:%S')
-    elif re.match(rb'^\d\d/\d\d/\d\d\d\d \d\d:\d\d:\d\d', s):
+    elif re.match(br'^\d\d/\d\d/\d\d\d\d \d\d:\d\d:\d\d', s):
         # 20/05/2014 00:00:00 EEST 
         # 18/08/2016 23:59:59
         return datetime.strptime(s[:19].decode('utf-8'), '%d/%m/%Y %H:%M:%S')
-    elif re.match(rb'^\d\d/\d\d/\d\d\d\d \d\d:\d\d:\d\d.\d\d\d\d\d\d [A-Z]+$', s):
+    elif re.match(br'^\d\d/\d\d/\d\d\d\d \d\d:\d\d:\d\d.\d\d\d\d\d\d [A-Z]+$', s):
         # 21/11/2013 23:21:16.461082 EET ... ignore TZ, guess it's nonstandard sometimes
         return datetime.strptime(s[:25].decode('utf-8'), '%d/%m/%Y %H:%M:%S.%f')
-    elif re.match(rb'^\d{2} [A-Z][a-z][a-z] \d{4} \d{2}:\d{2} [A-Z]+$', s):
+    elif re.match(br'^\d{2} [A-Z][a-z][a-z] \d{4} \d{2}:\d{2} [A-Z]+$', s):
         # 27 Jul 2014 00:00 UTC ... ignore sometimes nonstandard TZ
         return datetime.strptime(s[:17].decode('utf-8'), '%d %b %Y %H:%M')
-    elif re.match(rb'^[A-Z][a-z][a-z] [A-Z][a-z][a-z] \d\d \d\d:\d\d:\d\d [A-Z]+ \d\d\d\d$', s):
+    elif re.match(br'^[A-Z][a-z][a-z] [A-Z][a-z][a-z] \d\d \d\d:\d\d:\d\d [A-Z]+ \d\d\d\d$', s):
         # Sat Nov 02 23:59:59 GMT 2013
         return datetime.strptime(s.decode('utf-8'), '%a %b %d %H:%M:%S %Z %Y')
-    elif re.match(rb'^\d\d\d\d/\d\d/\d\d', s):
+    elif re.match(br'^\d\d\d\d/\d\d/\d\d', s):
         # 2022/09/25
         return datetime.strptime(s.decode('utf-8'), '%Y/%m/%d')
-    elif re.match(rb'^\d\d\d\d-\d\d-\d\d \d\d:\d\d:\d\d$', s):
+    elif re.match(br'^\d\d\d\d-\d\d-\d\d \d\d:\d\d:\d\d$', s):
         # 2014-03-18 08:09:30
         return datetime.strptime(s.decode('utf-8'), '%Y-%m-%d %H:%M:%S')
-    elif re.match(rb'^\d\d\d\d-\d\d-\d\d \d\d:\d\d:\d\d.\d+$', s):
+    elif re.match(br'^\d\d\d\d-\d\d-\d\d \d\d:\d\d:\d\d.\d+$', s):
         # 2007-01-28 11:14:49.12
         return datetime.strptime(s[:19].decode('utf-8'), '%Y-%m-%d %H:%M:%S')
-    elif re.match(rb'^\d\d-[A-Za-z][a-z][a-z]-\d\d\d\d\.?$', s):
+    elif re.match(br'^\d\d-[A-Za-z][a-z][a-z]-\d\d\d\d\.?$', s):
         # "30-jul-2014", "30-Jul-2014."
         return datetime.strptime(s[:11].decode('utf-8'), '%d-%b-%Y')
-    elif re.match(rb'^\d\d? [A-Za-z][a-z][a-z] \d\d\d\d$', s):
+    elif re.match(br'^\d\d? [A-Za-z][a-z][a-z] \d\d\d\d$', s):
         # 21 Jul 2015
         return datetime.strptime(s.decode('utf-8'), '%d %b %Y')
-    elif re.match(rb'^\d\d\d\d-\d\d-\d\dT\d\d:\d\d:\d\dZ$', s):
+    elif re.match(br'^\d\d\d\d-\d\d-\d\dT\d\d:\d\d:\d\dZ$', s):
         # 2014-03-18T04:09:30Z
         return datetime.strptime(s.decode('utf-8'), '%Y-%m-%dT%H:%M:%SZ')
-    elif re.match(rb'^\d\d\d\d\d\d\d\d$', s):
+    elif re.match(br'^\d\d\d\d\d\d\d\d$', s):
         # 20131217
         return datetime.strptime(s.decode('utf-8'), '%Y%m%d')
-    elif re.match(rb'^[A-Z][a-z]+ \d\d \d\d\d\d$', s):
+    elif re.match(br'^[A-Z][a-z]+ \d\d \d\d\d\d$', s):
         # February 13 2014
         return datetime.strptime(s.decode('utf-8'), '%B %d %Y')
-    elif re.match(rb'^[A-Z][a-z][a-z] [A-Z][a-z][a-z] [ \d]\d \d\d:\d\d:\d\d \d\d\d\d$', s):
+    elif re.match(br'^[A-Z][a-z][a-z] [A-Z][a-z][a-z] [ \d]\d \d\d:\d\d:\d\d \d\d\d\d$', s):
         # Tue Feb  4 23:59:00 2014
         return datetime.strptime(s.decode('utf-8'), '%a %b %d %H:%M:%S %Y')
-    elif re.match(rb'^\d\d\d\d\. \d\d\. \d\d\.$', s):
+    elif re.match(br'^\d\d\d\d\. \d\d\. \d\d\.$', s):
         # 2016. 10. 15.
         return datetime.strptime(s.decode('utf-8'), '%Y. %m. %d.')
-    elif re.match(rb'^\d{2}-[A-Z][a-z][a-z]-\d{4} \d{2}:\d{2}:\d{2}$', s):
+    elif re.match(br'^\d{2}-[A-Z][a-z][a-z]-\d{4} \d{2}:\d{2}:\d{2}$', s):
         # 23-Aug-2014 00:00:00
         return datetime.strptime(s.decode('utf-8'), '%d-%b-%Y %H:%M:%S')
-    elif re.match(rb'^\d\d-\d\d-\d\d\d\d', s):
+    elif re.match(br'^\d\d-\d\d-\d\d\d\d', s):
         # '14-06-2014  '
         return datetime.strptime(s[:10].decode('utf-8'), '%d-%m-%Y')
-    elif re.match(rb'^\d\d?\.\d\d?\.\d\d\d\d$', s):
+    elif re.match(br'^\d\d?\.\d\d?\.\d\d\d\d$', s):
         # 28.12.2013
         # 21.3.2015
         return datetime.strptime(s.decode('utf-8'), '%d.%m.%Y')
-    elif re.match(rb'^\d\d [A-Z][a-z][a-z] \d\d\d\d \d\d:\d\d:\d\d', s):
+    elif re.match(br'^\d\d [A-Z][a-z][a-z] \d\d\d\d \d\d:\d\d:\d\d', s):
         # 17 Mar 2014 13:13:54 UTC
         # 11 Feb 2014 01:07:00
         return datetime.strptime(s[:20].decode('utf-8'), '%d %b %Y %H:%M:%S')
-    elif re.match(rb'^\d\d\d\d-[A-Z][a-z][a-z]-\d\d\.', s):
+    elif re.match(br'^\d\d\d\d-[A-Z][a-z][a-z]-\d\d\.', s):
         # 2015-Feb-4.
         return datetime.strptime(s[:11].decode('utf-8'), '%Y-%b-%d')
-    elif re.match(rb'^\d\d\d\d-[A-Z][a-z][a-z]-\d\.', s):
+    elif re.match(br'^\d\d\d\d-[A-Z][a-z][a-z]-\d\.', s):
         # 2015-Feb-24.
         return datetime.strptime(s[:10].decode('utf-8'), '%Y-%b-%d')
-    elif re.match(rb'^\d\d/\d\d/\d\d\d\d$', s):
+    elif re.match(br'^\d\d/\d\d/\d\d\d\d$', s):
         # whois://whois.registry.pf/paea-musculation.org.pf - "Expire (JJ/MM/AAAA) : 07/08/2014"
         return datetime.strptime(s.decode('utf-8'), '%d/%m/%Y')
-    elif re.match(rb'^\d\d\d\d\.\d\d\.\d\d \d\d:\d\d:\d\d$', s):
+    elif re.match(br'^\d\d\d\d\.\d\d\.\d\d \d\d:\d\d:\d\d$', s):
         # whois://whois.dns.pl/rezerwuje.pl - "expiration date:       2014.03.21 19:47:27"
         return datetime.strptime(s.decode('utf-8'), '%Y.%m.%d %H:%M:%S')
-    elif re.match(rb'^\d\d\.\d\d\.\d\d\d\d \d\d:\d\d:\d\d$', s):
+    elif re.match(br'^\d\d\.\d\d\.\d\d\d\d \d\d:\d\d:\d\d$', s):
         # 18.10.2013 13:53:53
         return datetime.strptime(s.decode('utf-8'), '%d.%m.%Y %H:%M:%S')
     else:
@@ -118,7 +118,7 @@ def main(argv):
     line = fd.readline(8192)
     while line != b'':
         if state == WarcReadStates.EXPECTING_HEADER:
-            if re.match(rb'^WARC/\d+\.\d+\r\n$', line):
+            if re.match(br'^WARC/\d+\.\d+\r\n$', line):
                 state = WarcReadStates.IN_HEADER
             else:
                 raise StandardError('warc header not found where expected')
@@ -129,10 +129,10 @@ def main(argv):
                 content_bytes_read = 0
                 if content_length == None:
                     raise StandardError('Content-Length header not found')
-            m = re.match(rb'^(?i)Content-Length: (\d+)\r\n$', line)
+            m = re.match(br'^(?i)Content-Length: (\d+)\r\n$', line)
             if m != None:
                 content_length = int(m.group(1))
-            m = re.match(rb'^(?i)WARC-Target-URI: (.*)\r\n$', line)
+            m = re.match(br'^(?i)WARC-Target-URI: (.*)\r\n$', line)
             if m != None:
                 url = m.group(1)
 
@@ -147,7 +147,7 @@ def main(argv):
             state = WarcReadStates.EXPECTING_HEADER
 
         elif state == WarcReadStates.IN_CONTENT:
-            if url != None and url.startswith(b'whois://') and not re.match(rb'^.*\d+\.\d+\.\d+\.\d+$', url):
+            if url != None and url.startswith(b'whois://') and not re.match(br'^.*\d+\.\d+\.\d+\.\d+$', url):
                 # print('=== {0} ==='.format(record.url))
 
                 # Record expires on 30-Jul-2014.
@@ -163,12 +163,12 @@ def main(argv):
                 # whois://whois.jprs.jp/blogspot.jp 
                 # "[BM-8z4|8B]                      2013/10/31"
                 #
-                m = re.search(rb'(?im)(?:exp|valid|renewal)[^:\n]*?(?: on \s*:|:| on )\s*([^\r\n]+)', line)
+                m = re.search(br'(?im)(?:exp|valid|renewal)[^:\n]*?(?: on \s*:|:| on )\s*([^\r\n]+)', line)
 
                 if m != None:
                     d = parse_date(m.group(1))
                     if d != None:
-                        url_parts = re.split(rb'[/+]', url)
+                        url_parts = re.split(br'[/+]', url)
                         domain = url_parts[-1]
                         d14 = d.strftime('%Y%m%d%H%M%S')
                         print('{0} {1} {2}'.format(url.decode('utf-8'), domain.decode('utf-8'), d14))
