83436478bca0ed3a1cb70b86cf44c4274ab1b2d5
Kenji Nagahashi <kenji@archive.org>
2020-03-13 21:53:58 +0000

    Start version 0.2

--- wordpress/setup.py
+++ wordpress/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='no404-wordpress',
-    version='0.1.4',
+    version='0.2',
     author='Kenji Nagahashi',
     author_email='kenji@archive.org',
     description=("application to read wordpress update feed and schedule"
