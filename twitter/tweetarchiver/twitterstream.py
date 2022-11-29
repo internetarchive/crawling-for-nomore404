2b0383d42778043463c12fa34ad93f3092eb4b13
Anish Sarangi <anish@archive.org>
2022-11-30 01:14:05 +0530

    remove unused file and add entities field

--- twitter/tweetarchiver/twitterstream.py
+++ twitter/tweetarchiver/twitterstream.py
@@ -7,6 +7,6 @@ import json
 from twitter_stream import SampledStream
 
 class Stream(SampledStream):
-    user_fields = ['username']
+    user_fields = ['username','entities']
     expansions = ['author_id']
     tweet_fields = ['created_at']
