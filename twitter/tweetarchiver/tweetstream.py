d99ad2a7e21f315f02bce9b63bb7f48bc1ab0a73
Anish Sarangi <anish@archive.org>
2022-12-08 05:55:04 +0530

    add more expansion fields

--- twitter/tweetarchiver/tweetstream.py
+++ twitter/tweetarchiver/tweetstream.py
@@ -7,7 +7,7 @@ from configobj import ConfigObj
 
 
 def connect_to_endpoint(bearer_token):
-    expansions_fields = "author_id,referenced_tweets.id,attachments.media_keys,attachments.poll_ids,geo.place_id"
+    expansions_fields = "author_id,referenced_tweets.id,attachments.media_keys,attachments.poll_ids,geo.place_id,entities.mentions.username,in_reply_to_user_id,edit_history_tweet_ids	"
     user_fields = "created_at,description,entities,id,location,name,pinned_tweet_id,profile_image_url,protected,url,username,verified,withheld"
     tweet_fields = "attachments,author_id,context_annotations,created_at,entities,geo,id,in_reply_to_user_id,lang,possibly_sensitive,public_metrics,referenced_tweets,source,text,withheld"
     media_fields = "duration_ms,height,media_key,preview_image_url,public_metrics,type,url,width,alt_text"
